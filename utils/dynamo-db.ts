a9d8499ca52b9c88d46a36c2a0dfdcb5fba1c444
Serkan Haytac <serkanhaytac@IHM-SAT-SHAYTAC2.local>
2024-08-10 18:58:09 -0400

    updated

--- utils/dynamo-db.ts
+++ utils/dynamo-db.ts
@@ -1,22 +1,26 @@
-import AWS from 'aws-sdk';
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
 import { Message } from '../types';
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient({
+const dynamoDBClient = new DynamoDBClient({
   region: process.env.AWS_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
   endpoint: process.env.AWS_ENDPOINT,
 });
 
 export const saveMessages = async (sessionId: string, messages: Message[]) => {
   const params = {
     TableName: 'Conversations',
-    Item: {
+    Item: marshall({
       sessionId,
       messages,
       timestamp: new Date().toISOString(),
-    },
+    }),
   };
 
-  await dynamoDB.put(params).promise();
+  const command = new PutItemCommand(params);
+  await dynamoDBClient.send(command);
 };
